840197d3728dc29433262cb379aa8c7c3c6ce9bd
Austin Eldridge <deldrid1@vols.utk.edu>
2019-07-26 11:17:39 -0400

    Fix Windows Github remote relative include with __PATH__

--- src/Machine.js
+++ src/Machine.js
@@ -325,10 +325,15 @@ class Machine {
 
     // Check to see if file is a github absolute remote path, in which case we should return that path back directly
     if(this._getReader(includePath) === this.readers.github){
-      if(includePath.indexOf(context.__REPO_PREFIX__) > -1)
-        return context.__REPO_REF__ ? `${path.join(includePath)}@${context.__REPO_REF__}` : path.join(includePath); // Potentially someone using __PATH__
-      else
+      if(includePath.indexOf(context.__REPO_PREFIX__) > -1){
+        var rv = context.__REPO_REF__ ? `${path.join(includePath)}@${context.__REPO_REF__}` : path.join(includePath); // Potentially someone using __PATH__
+        if(process.platform === "win32"){
+          rv = rv.replace(/\\/g, '/');
+        }
+        return rv
+      } else{
         return includePath  // Absolute github include
+      }
     }
 
     // check if file is included from github source - if so, modify the path and return it relative to the repo root
